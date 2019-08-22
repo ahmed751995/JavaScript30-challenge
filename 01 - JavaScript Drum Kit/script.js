166575dc832317bc3a7a947ecee355ca3acb23fc
Ahmed Abdulrahman <ahmed@ahmed-Satellite-C50-A253>
2019-08-22 18:48:28 +0200

    edited drum kit to be displayed on mobile phone

--- 01 - JavaScript Drum Kit/script.js
+++ 01 - JavaScript Drum Kit/script.js
@@ -1,18 +1,39 @@
-window.addEventListener('keydown', playSound);
+window.addEventListener('keydown', playSoundKey);
 
-function playSound(e) {
+function playSoundKey(e) {
     let audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     let key = document.querySelector(`div[data-key="${e.keyCode}"]`);
-    if(!audio) return ;
-    audio.currentTime=0;
+    if (!audio) return;
+    audio.currentTime = 0;
     audio.play();
-    key.classList.add("play");
+    if (window.innerWidth > 800) {
+        key.classList.add("play");
+    }
+}
+
+
+//buttons
+
+let buttons = document.querySelectorAll('.button');
+buttons.forEach(button => button.addEventListener("click", playSoundButton));
+
+function playSoundButton(e) {
+    e.preventDefault();
+    let button = e.target;
+
+    let audio = document.querySelector(`audio[data-key="${button.getAttribute('data-key')}"]`);
+
+    if (!audio) return;
+    audio.currentTime = 0;
+    audio.play();
 }
 
 let keys = document.querySelectorAll('.key')
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
 
+
+
 function removeTransition(e) {
-    if(e.propertyName !== 'transform') return;
+    if (e.propertyName !== 'transform') return;
     e.target.classList.remove('play');
 }
