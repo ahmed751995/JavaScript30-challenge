9fad6bd203b2619b88fb2e59d895648c432aa97e
Ahmed Abdulrahman <ahmed751995@gmail.com>
2019-09-07 02:11:27 +0200

    minor update to Webcam Fun

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -1,5 +1,5 @@
-const video = document.querySelector('video'); //.player
-const canvas = document.querySelector('canvas'); //.photo
+const video = document.querySelector('.player'); 
+const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
