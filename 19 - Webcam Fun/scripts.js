b4a8aeeb22a83d3395ddc5e63018c335fbda0d56
Ahmed Abdulrahman <ahmed751995@gmail.com>
2019-09-07 02:09:48 +0200

    updated Webcam app

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -1,55 +1,91 @@
-const video = document.querySelector('.player');
-const canvas = /** @type {HTMLCanvasElement} */document.querySelector('.photo');
+const video = document.querySelector('video'); //.player
+const canvas = document.querySelector('canvas'); //.photo
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+let effect = 'normal-effect';
 
-
-function getVideo() {
-    navigator.mediaDevices.getUserMedia({video: true, audio: false})
-    .then(localMediaStream => {
-        video.srcObject = localMediaStream;
+function playVideo() {
+    navigator.mediaDevices.getUserMedia({video:true, audio:false})
+    .then( stream => {
+        video.srcObject = stream;
         video.play();
-    })
-    .catch(err => {
-        console.error('please allow access to your camera',err);
     });
 }
 
-
-function paintToCanvas() {
-    const width = video.videoWidth;
-    const height = video.videoHeight;
-    canvas.width = width;
+function drawToCanvas() {
+    let height = video.videoHeight;
+    let width = video.videoWidth;
     canvas.height = height;
-    
+    canvas.width = width;
+
     return setInterval(() => {
-        ctx.drawImage(video, 0,0, width, height);
-        let pixels = ctx.getImageData(0,0,width, height);
-        pixels = redEffect(pixels);  
-        ctx.putImageData(pixels, 0, 0);      
-    }, 16);
+        ctx.drawImage(video, 0,0,width, height);
+        if(effect == 'normal-effect') {
+            
+        }
+        else{
+            let pixels = ctx.getImageData(0,0, width, height);
+        
+            if(effect == 'alpha-effect') {
+                pixels = alphaEffect(pixels);
+            }
+         else if(effect == 'red-effect') {
+                pixels = redEffect(pixels);
+            }
+            ctx.putImageData(pixels, 0,0);
+        }
+    },16);
 }
 
 function takePhoto() {
     snap.currentTime = 0;
     snap.play();
-
-    let data = canvas.toDataURL('image/jpeg');
-    let link = document.createElement('a');
-    link.href = data;
-    link.setAttribute('download', 'handsome');
-    link.innerHTML = `<img src=${data} alt="someone"></img>`;
-    strip.insertBefore(link, strip.firstChild);
+    let link = canvas.toDataURL('image/jpeg');
+    let a = document.createElement('a');
+    a.setAttribute("href", link);
+    a.innerHTML = `<img src=${link} placeholder="handsome man">`;
+    strip.insertBefore(a, strip.firstChild);
 }
 
-
-getVideo();
-video.addEventListener('canplay', paintToCanvas); 
+playVideo();
 
 function redEffect(pixels) {
-    for(let i = 0; i< pixels.data.length; i+= 4) {
+    for(let i = 0; i < pixels.data.length; i+= 4) {
         pixels.data[i] = pixels.data[i] + 100;
     }
     return pixels;
 }
+
+function alphaEffect(pixels) {
+    let inputs = {};
+    document.querySelectorAll('.rgb input').forEach(input => {
+       inputs[input.name] = input.value;
+    });
+    let red, green, blue;
+
+    for(let i = 0; i < pixels.data.length; i+=4) {
+        red = pixels.data[i];
+        green = pixels.data[i + 1];
+        blue = pixels.data[i + 2];
+
+        if (red >= inputs.rmin
+            && green >= inputs.gmin
+            && blue >= inputs.bmin
+            && red <= inputs.rmax
+            && green <= inputs.gmax
+            && blue <= inputs.bmax) {
+                pixels.data[i + 3] = 0;
+            }
+    }
+    return pixels;
+}
+
+alphaEffect({data:[1,2,3,4]});
+
+video.addEventListener('canplay', drawToCanvas);
+
+document.querySelectorAll('input[type="radio"]')
+.forEach(radio => {
+    radio.addEventListener('change', () => effect = radio.value);
+});
