baa8d8b54204f1c21a3c81d7e73b0f7ee839f382
Ahmed Abdulrahman <ahmed751995@gmail.com>
2019-09-07 02:18:08 +0200

    beautify Webcam Fun code

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -1,4 +1,4 @@
-const video = document.querySelector('.player'); 
+const video = document.querySelector('.player');
 const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
@@ -6,11 +6,14 @@ const snap = document.querySelector('.snap');
 let effect = 'normal-effect';
 
 function playVideo() {
-    navigator.mediaDevices.getUserMedia({video:true, audio:false})
-    .then( stream => {
-        video.srcObject = stream;
-        video.play();
-    });
+    navigator.mediaDevices.getUserMedia({
+            video: true,
+            audio: false
+        })
+        .then(stream => {
+            video.srcObject = stream;
+            video.play();
+        });
 }
 
 function drawToCanvas() {
@@ -20,22 +23,20 @@ function drawToCanvas() {
     canvas.width = width;
 
     return setInterval(() => {
-        ctx.drawImage(video, 0,0,width, height);
-        if(effect == 'normal-effect') {
-            
-        }
-        else{
-            let pixels = ctx.getImageData(0,0, width, height);
-        
-            if(effect == 'alpha-effect') {
+        ctx.drawImage(video, 0, 0, width, height);
+        if (effect == 'normal-effect') {
+
+        } else {
+            let pixels = ctx.getImageData(0, 0, width, height);
+
+            if (effect == 'alpha-effect') {
                 pixels = alphaEffect(pixels);
-            }
-         else if(effect == 'red-effect') {
+            } else if (effect == 'red-effect') {
                 pixels = redEffect(pixels);
             }
-            ctx.putImageData(pixels, 0,0);
+            ctx.putImageData(pixels, 0, 0);
         }
-    },16);
+    }, 16);
 }
 
 function takePhoto() {
@@ -51,7 +52,7 @@ function takePhoto() {
 playVideo();
 
 function redEffect(pixels) {
-    for(let i = 0; i < pixels.data.length; i+= 4) {
+    for (let i = 0; i < pixels.data.length; i += 4) {
         pixels.data[i] = pixels.data[i] + 100;
     }
     return pixels;
@@ -60,23 +61,23 @@ function redEffect(pixels) {
 function alphaEffect(pixels) {
     let inputs = {};
     document.querySelectorAll('.rgb input').forEach(input => {
-       inputs[input.name] = input.value;
+        inputs[input.name] = input.value;
     });
     let red, green, blue;
 
-    for(let i = 0; i < pixels.data.length; i+=4) {
+    for (let i = 0; i < pixels.data.length; i += 4) {
         red = pixels.data[i];
         green = pixels.data[i + 1];
         blue = pixels.data[i + 2];
 
-        if (red >= inputs.rmin
-            && green >= inputs.gmin
-            && blue >= inputs.bmin
-            && red <= inputs.rmax
-            && green <= inputs.gmax
-            && blue <= inputs.bmax) {
-                pixels.data[i + 3] = 0;
-            }
+        if (red >= inputs.rmin &&
+            green >= inputs.gmin &&
+            blue >= inputs.bmin &&
+            red <= inputs.rmax &&
+            green <= inputs.gmax &&
+            blue <= inputs.bmax) {
+            pixels.data[i + 3] = 0;
+        }
     }
     return pixels;
 }
@@ -84,6 +85,6 @@ function alphaEffect(pixels) {
 video.addEventListener('canplay', drawToCanvas);
 
 document.querySelectorAll('input[type="radio"]')
-.forEach(radio => {
-    radio.addEventListener('change', () => effect = radio.value);
-});
+    .forEach(radio => {
+        radio.addEventListener('change', () => effect = radio.value);
+    });
